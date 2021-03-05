89e42e90b5d7740887163244cf0513d8571fe4a0
Liza <liza.guyvan1@gmail.com>
2021-03-05 12:45:28 +0100

    search, select added

--- src/components/CountryList.js
+++ src/components/CountryList.js
@@ -6,7 +6,14 @@ export const CountryList = ({ countries }) => {
     return (
         <div className="country-list">
             <input className=" fa country-list__search" placeholder="&#xF002; Search for a counrty..." />
-            <div className="country-list__filter"></div>
+            <select className="country-list__filter">
+                <option value="All">Filter by region</option>
+                <option value="Africa">Africa</option>
+                <option value="America">America</option>
+                <option value="Asia">Asia</option>
+                <option value="Europe">Europe</option>
+                <option value="Oceania">Oceania</option>
+            </select>
             <div className="country-list__container">
             {countries.map((country, index) => (
                     <CountryCard key={index} name={country.name} population={country.population} region={country.region} capital={country.capital} flag={country.flag}></CountryCard>
